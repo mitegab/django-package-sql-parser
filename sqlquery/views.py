456bdd2c38ce06f67ca5be448ac9606ad9aaa489
mitegab <104057957+mitegab@users.noreply.github.com>
2024-04-17 11:50:56 +0300

    merged the url and the view

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -2,14 +2,14 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Product
-from django.core.serializers import serializer
+from django.core.serializers import serialize
 
 # Create your views here.
 
 def home(request):
     qs = Product.objects.all()
-    serialized_data =serializer('json', qs)
+    serialized_data =serialize('json', qs)
     serialized_data = json.loads(serialized_data)
-    return JsonResponse(serialized_data, status=200)
+    return JsonResponse(serialized_data, safe=False, status=200)
 
     
