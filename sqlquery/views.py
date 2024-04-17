e893856c71e6c2b342bb9f54ed5d6d432b5f3bd5
mitegab <104057957+mitegab@users.noreply.github.com>
2024-04-17 11:41:39 +0300

    update on the view.py

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -1,6 +1,15 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
+from django.core.serializers import serializer
 
 # Create your views here.
 
 def home(request):
-    pass
+    qs = Product.objects.all()
+    serialized_data =serializer('json', qs)
+    serialized_data = json.loads(serialized_data)
+    return JsonResponse(serialized_data, status=200)
+
+    
