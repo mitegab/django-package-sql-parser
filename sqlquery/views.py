851e7d0b4c95456096b1bbaedd5f05cb52efc099
mitegab <104057957+mitegab@users.noreply.github.com>
2024-04-17 13:27:35 +0300

    made the code, now it prints the sql queries on the terminal

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Product
 from django.core.serializers import serialize
-
+from django.db import connection
 # Create your views here.
 
 def home(request):
     qs = Product.objects.all()
+
+    
+    
     serialized_data =serialize('json', qs)
+    print(connection.queries)
     serialized_data = json.loads(serialized_data)
     return JsonResponse(serialized_data, safe=False, status=200)
 
